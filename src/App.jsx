e9aea62d071fbf2fa10fd23fa7283b9942c55dc2
RU063 <rashedinislam.06@gmail.com>
2024-09-30 17:36:19 +0600

    font, background, favicon,title setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
 const App = () => {
   return (
-    <div className="text-center text-red-500">App</div>
-  )
+    <div className='text-center text-red-500 b min-h-screen flex justify-center items-center bg-custom-gradient text-3xl font-semibold '>
+      App
+    </div>
+  );
 }
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+       monospace: "'Space Mono', monospace;"
+      },
+      backgroundImage: {
+        'custom-gradient':
+          'linear-gradient(to right, #035762,  #0C716A, #13826F,  #23AF7D, #38C8B6)',
+      },
+      colors: {
+        'primary-green': '#1b9876',
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
