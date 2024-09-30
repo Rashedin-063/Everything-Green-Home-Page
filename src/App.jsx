c8a66da779ab54177351bdb1d3c332301121b417
RU063 <rashedinislam.06@gmail.com>
2024-09-30 14:47:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+
+const App = () => {
+  return (
+    <div className="text-center text-red-500">App</div>
+  )
+}
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+
+
+const Home = () => {
+  return (
+    <div className='text-center text-red-500'>
+      <h1 className="text-red-600500">Home</h1>
+    </div>
+  );
+}
+export default Home
